bb96240bb6bcc3271d761baf2105de520d1fc888
Aidan M <asmaidan@outlook.com>
2024-01-26 14:50:55 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-cd subscout && cargo build --release && cp /home/zdroid/Documents/Pentesting/pentest/subscout/target/release/subscout /home/zdroid/Documents/Pentesting/pentest/src/
+cargo build --release && cp /pentest/target/release/pentest /pentest/binaries/
